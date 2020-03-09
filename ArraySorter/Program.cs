aa803b30eaa68d3502c9c32cc3fc62a7f7e2076b
Jack-Borg <jacob.fam.borg@gmail.com>
2020-03-09 11:56:04 +0100

    first commit

---
+++ ArraySorter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ArraySorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str = {"e", "b", "a", "y", "f", "g", "u"};
+
+            ArraySorter<String> test = new ArraySorter<string>(str, str.Length);
+    
+            //String[] arr = ArraySorter<String>.Sort(str);
+
+            test.enqueue("bob");
+            
+            /*foreach (string s in arr)
+            {
+                Console.Write(s + " ");
+            }*/
+
+            Console.WriteLine();
+        }
+
+        //a b e f g u y
+    }
+
+    /*        private static bool Less(String s1, String s2)
+        {
+            char[] ca1 = s1.ToCharArray();
+            char[] ca2 = s2.ToCharArray();
+            int cycles = ca1.Length > ca2.Length ? ca2.Length : ca1.Length;
+            for (int i = 0; i < cycles; i++)
+            {
+                if (ca1[i] != ca2[i])
+                {
+                    return ca1[i] - 97 < ca2[i] - 97;
+                }
+            }
+
+            return s2.Length > s1.Length;
+        }*/
+}
---
+++ ArraySorter/Util.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArraySorter
+{
+    public class Util<T>
+    {
+        public Util()
+        {
+        }
+
+        public T[] Switch(T[] a, int i, int j)
+        {
+            T tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            return a;
+        }
+
+        public T[] IncreaseArray(T[] items, T item, int size)
+        {
+            T[] tmp = items;
+            items = new T[size];
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                items[i] = tmp[i];
+            }
+
+            items[items.Length - 1] = item;
+
+            return items;
+        }
+    }
+}
